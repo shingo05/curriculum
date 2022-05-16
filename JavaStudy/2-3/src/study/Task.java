9940d6267875b441aaaefe64b43e2142e398a34d
shingo05 <nishidash.vntn01@gmail.com>
2022-05-16 23:24:16 +0900

    課題提出テキスト

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -10,15 +10,14 @@ public class Task extends Calculator {
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	Calculator cal = new Calculator();
-    		int cal1 = cal.plus(10);
-    		System.out.println("plusメソッドの引数が一つの場合：" + cal1);
+    		super.plus(10);
+    		System.out.println("plusメソッドの引数が一つの場合：" + super.plus(10));
     
-    		int cal2 = cal.plus(10, 20);
-    		System.out.println("plusメソッドの引数が二つの場合：" + cal2);
+    		super.plus(10, 20);
+    		System.out.println("plusメソッドの引数が二つの場合：" + super.plus(10,20));
 
-			int cal3 = cal.plus(10, 20, 30);
-			System.out.println("plusメソッドの引数が三つの場合：" + cal3);
+			super.plus(10, 20, 30);
+			System.out.println("plusメソッドの引数が三つの場合：" + super.plus(10,20,30));
 
     }
 	
