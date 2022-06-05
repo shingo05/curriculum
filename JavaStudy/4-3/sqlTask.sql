642ce7eb48376fcfad1935704befcebdebf3c6e2
shingo05 <nishidash.vntn01@gmail.com>
2022-06-05 09:35:36 +0900

    Update sqlTask.sql

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -26,14 +26,14 @@
 -- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
 -- INSERT INTO goods_table
 -- (goods_code, goods_name, price, update_day)
--- VALUES('M001','マフラー','4500','2022-06-01');
+-- VALUES('M001','マフラー','4500',CURRENT_DATE);
  
 
 -- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
 -- UPDATE stock_table
 -- SET
 -- quantity = '10',
--- update_day = '2022-06-01'
+-- update_day = CURRENT_DATE
 -- WHERE
 -- goods_code = 'S987'
 -- AND
